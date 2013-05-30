00be0a299dd9abc3f2bfd3b5081ec0dd125ec8a7
Guillermo Carrasco <guillermo.carrasco@scilifelab.se>
2013-05-30 17:48:48 +0000

    Recipe to install the latest code

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['pydoop-latest-code']   =   'http://git.code.sf.net/p/pydoop/code pydoop'
+default['pydoop-latest-code']   =   'http://git.code.sf.net/p/pydoop/code'
 default['tmp']                  =   '/tmp/pydoop'
 default['install_dir']          =   '/opt'
---
+++ recipes/latest_code.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: pydoop
+# Recipe:: latest_code
+#
+
+#include_recipe "hadoop"
+include_recipe "pydoop-cookbook::install_deps"
+
+#Checkot pydoop from Git
+git "Checking out hadoop-BAM code" do
+    repository  "#{node['pydoop-latest-code']}"
+    action :sync
+    destination "#{node['install_dir']}/pydoop"
+end
+
+#Compile Hadoop-BAM. Requires java SDK!
+execute "Compiling and installing pydoop" do
+    command "cd #{node['install_dir']}/pydoop && python setup.py build && python setup.py install"
+    action :run
+end
+
