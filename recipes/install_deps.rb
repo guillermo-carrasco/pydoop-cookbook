89cd7f9ffcd2c4340612d403d377fb6ada3b5a76
Guillermo Carrasco <guillermo.carrasco@scilifelab.se>
2013-05-30 17:10:24 +0000

    Install common dependencies

---
+++ recipes/install_deps.rb
@@ -0,0 +1,9 @@
+#Install pydoop dependencies
+
+package "libboost-python-dev"
+package "python-support"
+package "build-essential"
+package "python-all-dev"
+package "libssl-dev"
+package "hadoop-client"
+package "python-pip"
