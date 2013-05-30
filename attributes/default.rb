37b47478f87032da728fb9115cbde526df3ab12d
Guillermo Carrasco <guillermo.carrasco@scilifelab.se>
2013-05-30 16:24:58 +0000

    First commit (typical message...)

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['pydoop-release']       =   'http://sourceforge.net/projects/pydoop/files/latest/download'
+default['pydoop-latest-code']   =   'http://git.code.sf.net/p/pydoop/code pydoop'
+default['tmp']                  =   '/tmp/pydoop'
+default['install_dir']          =   '/opt'
